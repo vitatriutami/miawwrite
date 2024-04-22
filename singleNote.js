e85c07f968ffb2352e5e1674c3f707f498b12366
Vita <vitatriutami@gmail.com>
2024-04-22 09:14:32 +0700

    update noteJS

--- singleNote.js
+++ singleNote.js
@@ -8,10 +8,10 @@ const id = params.get("id");
 
 const API_ENDPOINT = `https://v1.appbackend.io/v1/rows/768NKI6qq7pq/${id}`;
 
-editBtn.href = `editNote.html?id=${note._id}`;
-deleteBtn.addEventListener("click", async () => {
-  await deleteNote(note._id);
-});
+// editBtn.href = `editNote.html?id=${note._id}`;
+// deleteBtn.addEventListener("click", async () => {
+//   await deleteNote(note._id);
+// });
 
 async function getNote() {
   const res = await fetch(API_ENDPOINT);
@@ -21,7 +21,6 @@ async function getNote() {
 
 async function buildApp() {
   const note = await getNote();
-  const noteCard = document.createElement("div");
   const title = document.createElement("h2");
   const content = document.createElement("p");
   const date = document.createElement("p");
